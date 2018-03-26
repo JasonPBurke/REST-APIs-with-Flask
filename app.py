c87985748c0ad9db20d794d6e569a1e0c0a6d305
Jason Burke <jpburke77@gmail.com>
2018-03-26 12:00:31 -0500

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turns off flask_sqlalchem
 app.secret_key = 'aSecretKey'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) # creates a new endpiont.. /auth
 
 api.add_resource(Store, '/store/<string:name>')
